dcd07c752e5e9ef29a683e86303aa23ad2662e5c
krishna chaitanya <arekapudikrishnachaitanya@gmail.com>
2024-10-01 16:06:52 +0530

    responsiveness added completely

--- src/Pages/WebDevPage/Components/TextPart/TextPart.jsx
+++ src/Pages/WebDevPage/Components/TextPart/TextPart.jsx
@@ -1,49 +1,53 @@
 import React from 'react'
 import './TextPart.css'
+import { body } from 'framer-motion/client'
 const TextPart = () => {
   return (
+
     <div className='TextPartContainer'>
-      <h1>Software Development </h1>
-      <div className="text">
+      
+        <h1>Software Development </h1>
+        <div className="text">
 
-        <h4><b>Custom Software Development Services Tailored to Your Business Needs</b></h4> 
+          <h4><b>Custom Software Development Services Tailored to Your Business Needs</b></h4>
 
-        <p>Our IT professionals specialize in developing robust software and web applications that are seamlessly integrated and optimized across all platforms. Whether you need to upgrade legacy systems or build new mobile, web, or desktop applications, our expert teams are here to deliver.</p> 
+          <p>Our IT professionals specialize in developing robust software and web applications that are seamlessly integrated and optimized across all platforms. Whether you need to upgrade legacy systems or build new mobile, web, or desktop applications, our expert teams are here to deliver.</p>
 
-        <h4>
-          <b>
-            Choosing the Right Technology for Your Project
-          </b>
-        </h4>
-
-
-        <p>
-          Not sure which technology stack is the best fit for your project? Don’t worry—we’ve got you covered. Our experienced developers will guide you in selecting the ideal front-end and back-end technologies, ensuring a complete end-to-end solution tailored to your business goals.
-        </p>
-        
-        <h4>
-
-          <b>
-            Agile Development for Fast, Collaborative Results
-          </b>
-        </h4>
-        
+          <h4>
+            <b>
+              Choosing the Right Technology for Your Project
+            </b>
+          </h4>
 
 
-        <p>
-          We believe in the power of agile development practices to bring your software to life quickly and efficiently. Our team partners with you every step of the way, from design to deployment, to create custom software that powers your business forward.
-        </p>
-        
-        <h4>
+          <p>
+            Not sure which technology stack is the best fit for your project? Don’t worry—we’ve got you covered. Our experienced developers will guide you in selecting the ideal front-end and back-end technologies, ensuring a complete end-to-end solution tailored to your business goals.
+          </p>
 
-          <b>
-            Offshore Software Product Engineering
-          </b>
-        </h4>
-        <p>
-          Looking for advanced and cost-effective solutions? Our offshore software product engineering team delivers cutting-edge technology tailored to meet your specific requirements. We work closely with you to ensure the final product aligns perfectly with your vision and business objectives.
-        </p>
-      </div>
+          <h4>
+
+            <b>
+              Agile Development for Fast, Collaborative Results
+            </b>
+          </h4>
+
+
+
+          <p>
+            We believe in the power of agile development practices to bring your software to life quickly and efficiently. Our team partners with you every step of the way, from design to deployment, to create custom software that powers your business forward.
+          </p>
+
+          <h4>
+
+            <b>
+              Offshore Software Product Engineering
+            </b>
+          </h4>
+          <p>
+            Looking for advanced and cost-effective solutions? Our offshore software product engineering team delivers cutting-edge technology tailored to meet your specific requirements. We work closely with you to ensure the final product aligns perfectly with your vision and business objectives.
+          </p>
+        </div>
+      
     </div>
   )
 }
