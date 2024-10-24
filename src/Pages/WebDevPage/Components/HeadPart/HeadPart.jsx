191f81eaf29a6b8c54863df08b097e06169123c5
krishna chaitanya <arekapudikrishnachaitanya@gmail.com>
2024-10-24 15:01:00 +0530

    done

--- src/Pages/WebDevPage/Components/HeadPart/HeadPart.jsx
+++ src/Pages/WebDevPage/Components/HeadPart/HeadPart.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import './HeadPart.css'
+import React from "react";
+import "./HeadPart.css";
 const HeadPart = () => {
   return (
-    <div className='headPartContainer'>
-        <h3>services</h3>
+    <div className="headPartContainer">
+      <h3></h3>
     </div>
-  )
-}
+  );
+};
 
-export default HeadPart
+export default HeadPart;
